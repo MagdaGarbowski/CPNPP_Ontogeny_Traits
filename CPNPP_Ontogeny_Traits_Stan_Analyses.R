3d3d1231479a4db931e9b743516cfbd8bb20a21b
Magda Garbowski <mgarbow@rams.colostate.edu>
2020-02-13 16:41:14 -0700

    Trying to write a function to call stan model

--- CPNPP_Ontogeny_Traits_Stan_Analyses.R
+++ CPNPP_Ontogeny_Traits_Stan_Analyses.R
@@ -1,11 +1,9 @@
 setwd("/Users/MagdaGarbowski/CPNPP_Ontogeny_Traits/")
 SpeciesData<-read.csv("Data_Generated/TraitData_2017.csv")
 source("Rscripts/Functions/Functions_Stan_Analyses.R")
+file.exists("stan_models/IndividualTraitbySpecies.stan")
+library(rstan)
 
-SpeciesData$RMR_100<-SpeciesData$RMR*100
-SpeciesData$RDMC_100<-SpeciesData$RDMC*100
-SpeciesData$LDMC_100<-SpeciesData$LDMC*100
-SpeciesData$RTD_100<-SpeciesData$RTD*100
 SpeciesData$H_num<-as.character(as.factor(SpeciesData$H_num))
 
 # Add NA for H1 SLA for forb species 
@@ -22,49 +20,46 @@ mk_data_RMR<-lapply(dat_na.omit_RMR, mk_data_function, "RMR")
 
 # ----------------------------- Run SLA models one by one to see if they are working. Divergent transitions for most -------------------------------------------------------
 
-fit_SLA_ACMI <- stan(file = "stan_models/example.stan", data = mk_data_SLA$ACMI, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) #  Divergent transitions
-summary(fit_SLA_ACMI) # MATT - why is it printing summaries for all chains? 
-aggregate(.~H_num, dat_na.omit_SLA$ACMI, FUN = mean)
+SLA_mods_function<-function(df){
+  stan(file = "stan_models/IndividualTraitbySpecies.stan", df, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) # Not working... 
+}
 
-fit_SLA_ARTR <- stan(file = "stan_models/example.stan", data = mk_data_SLA$ARTR, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) #  Divergent transtitions
+SLA_mods_function(mk_data_SLA$ACMI)
+
+# -----------------------------------------------
+
+fit_SLA_ACMI <- stan(file = "stan_models/IndividualTraitbySpecies.stan", data = mk_data_SLA$ACMI, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99))
+summary(fit_SLA_ACMI)$summary
+
+fit_SLA_ARTR <- stan(file = "stan_models/IndividualTraitbySpecies.stan", data = mk_data_SLA$ARTR, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99))
 summary(fit_SLA_ARTR) 
-aggregate(.~H_num, dat_na.omit_SLA$ARTR, FUN = mean)
 
-fit_SLA_ELTR <- stan(file = "stan_models/example.stan", data = mk_data_SLA$ELTR, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) # OK 
+fit_SLA_ELTR <- stan(file = "stan_models/IndividualTraitbySpecies.stan", data = mk_data_SLA$ELTR, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99))
 summary(fit_SLA_ELTR) 
-aggregate(.~H_num, dat_na.omit_SLA$ELTR, FUN = mean)
 
-fit_SLA_HECO <- stan(file = "stan_models/example.stan", data = mk_data_SLA$HECO, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) #  Divergent transtitions
+fit_SLA_HECO <- stan(file = "stan_models/IndividualTraitbySpecies.stan", data = mk_data_SLA$HECO, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) 
 summary(fit_SLA_HECO) 
-aggregate(.~H_num, dat_na.omit_SLA$HECO, FUN = mean)
 
-fit_SLA_HEAN <- stan(file = "stan_models/example.stan", data = mk_data_SLA$HEAN, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) #  Divergent transtitions
+fit_SLA_HEAN <- stan(file = "stan_models/IndividualTraitbySpecies.stan", data = mk_data_SLA$HEAN, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) 
 summary(fit_SLA_HEAN) 
-aggregate(.~H_num, dat_na.omit_SLA$HEAN, FUN = mean)
 
 fit_SLA_HEVI <- stan(file = "stan_models/example.stan", data = mk_data_SLA$HEVI, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) # Divergent transtitions
 summary(fit_SLA_HEVI) 
-aggregate(.~H_num, dat_na.omit_SLA$HEVI, FUN = mean)
 
 fit_SLA_MUPO <- stan(file = "stan_models/example.stan", data = mk_data_SLA$MUPO, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) # Divergent transtitions
 summary(fit_SLA_MUPO) 
-aggregate(.~H_num, dat_na.omit_SLA$MUPO, FUN = mean)
 
 fit_SLA_MACA <- stan(file = "stan_models/example.stan", data = mk_data_SLA$MACA, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) # Divergent transitions
 summary(fit_SLA_MACA) 
-aggregate(.~H_num, dat_na.omit_SLA$MACA, FUN = mean)
 
 fit_SLA_PAMU <- stan(file = "stan_models/example.stan", data = mk_data_SLA$PAMU, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) # Divergent transitions
 summary(fit_SLA_PAMU) 
-aggregate(.~H_num, dat_na.omit_SLA$PAMU, FUN = mean)
 
 fit_SLA_PLPA <- stan(file = "stan_models/example.stan", data = mk_data_SLA$PLPA, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) # Divergent transitions
 summary(fit_SLA_PLPA) 
-aggregate(.~H_num, dat_na.omit_SLA$PLPA, FUN = mean)
 
 fit_SLA_VUOC <- stan(file = "stan_models/example.stan", data = mk_data_SLA$VUOC, warmup = 500, iter = 3000, chains = 4, cores = 2, thin = 1, control = list(adapt_delta = 0.99)) # Divergent transitions
 summary(fit_SLA_VUOC) 
-aggregate(.~H_num, dat_na.omit_SLA$VUOC, FUN = mean)
 
 
 # ----------------------------- Run RMR models one by one to see if they are working. Divergent transitions for most -------------------------------------------------------
