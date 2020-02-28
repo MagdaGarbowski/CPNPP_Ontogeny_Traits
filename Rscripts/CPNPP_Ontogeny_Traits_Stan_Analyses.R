55322dc2c83ff8b8cfff3acd3251a75e1a680725
Magda Garbowski <mgarbow@rams.colostate.edu>
2020-02-28 15:52:11 -0700

    Estimating differences, back transforming par values for plotting - doing this right?

--- Rscripts/CPNPP_Ontogeny_Traits_Stan_Analyses.R
+++ Rscripts/CPNPP_Ontogeny_Traits_Stan_Analyses.R
@@ -1,34 +1,29 @@
 if(Sys.info()["login"] == "MagdaGarbowski")
     setwd("/Users/MagdaGarbowski/CPNPP_Ontogeny_Traits/")
 
-SpeciesData<-read.csv("Data_Generated/TraitData_2017.csv")
 TraitData_Pop_Avg_2017<-read.csv("Data_Generated/TraitData_PopAvg_2017.csv")
+Pop_avg_data_wrates<-read.csv("Data_Generated/TraitData_PopAvg_wRates_2017.csv")
 
 source("Rscripts/Functions/Functions_Stan_Analyses.R")
 library(rstan)
 library(bayesplot)
+library(HDInterval)
 bayesplot::color_scheme_set("brightblue")
 
-
-SpeciesData$H_num<-as.character(as.factor(SpeciesData$H_num))
-SpeciesData$SLA<-ifelse((SpeciesData$GrowthForm %in% c("FORB","SHRUB") & SpeciesData$H_num == "H1"), NA, SpeciesData$SLA) # Add NA for H1 SLA for forb species 
-SpeciesData$SLA<-ifelse((SpeciesData$SLA == 0), NA, SpeciesData$SLA)
-Species_splits = split(SpeciesData, paste(SpeciesData$SPECIES))
-
-TraitData_Pop_Avg_2017$H_num<-as.character(as.factor(TraitData_Pop_Avg_2017$H_num))
-TraitData_Pop_Avg_2017$value<-ifelse((TraitData_Pop_Avg_2017$GrowthForm %in% c("FORB","SHRUB", "GRASS") & TraitData_Pop_Avg_2017$H_num == "H1" & TraitData_Pop_Avg_2017$trait %in% c("SLA", "LDMC")), NA, TraitData_Pop_Avg_2017$value)
+Pop_avg_data_wrates$H_num<-as.character(as.factor(Pop_avg_data_wrates$H_num))
+Pop_avg_data_wrates$value<-ifelse((Pop_avg_data_wrates$GrowthForm %in% c("FORB","SHRUB", "GRASS") & Pop_avg_data_wrates$H_num == "H1" & Pop_avg_data_wrates$trait %in% c("ln.SLA", "ln.LDMC", "value.SLA","value.LDMC")), NA, Pop_avg_data_wrates$value)
 
 # Data for variance estimates of traits
-na.omit_traits<-na.omit.fun(TraitData_Pop_Avg_2017, c("POP_ID", "value","trait","SPECIES"))
-traits_stan_df<-mk_data_traitvar_function(na.omit_traits)
+# na.omit_traits<-na.omit.fun(TraitData_Pop_Avg_2017, c("POP_ID", "value","trait","SPECIES"))
+# traits_stan_df<-mk_data_traitvar_function(na.omit_traits)
 
 # Data for full model - mean estimates of traits by species and time 
-na.omit_species<-na.omit.fun(TraitData_Pop_Avg_2017, c("POP_ID", "value","trait","SPECIES", "H_num"))
-Traits_all<-na.omit_species[na.omit_species$trait %in% c ("RDMC","RTD","RMR","SRL"),]
-Traits_leaf<-na.omit_species[na.omit_species$trait %in% c ("SLA","LDMC"),]
+na.omit_species<-na.omit.fun(Pop_avg_data_wrates, c("POP_ID", "value","trait","SPECIES", "H_num"))
+Traits_all<-na.omit_species[na.omit_species$trait %in% c ("ln.RDMC","ln.RTD","ln.RMR","ln.SRL"),]
+Traits_noH1<-na.omit_species[na.omit_species$trait %in% c ("ln.SLA","ln.LDMC", "RER_ln","RGR_Tot_ln"),]
 
 Trait_splits_full = split(Traits_all, paste(Traits_all$trait))
-Trait_splits_noH1<-split(Traits_leaf, paste(Traits_leaf$trait))
+Trait_splits_noH1<-split(Traits_noH1, paste(Traits_leaf$trait))
 
 
 ################################################################################
@@ -39,10 +34,10 @@ data_noH1<-lapply(Trait_splits_noH1, prep_data)
 
 # Full model (Species and H_num together) --------------------------------------#
 # Will still need to add random effect of population 
-mod = stan_model("stan_models/All_Traits_Random_Effects_HS.stan")
+mod = stan_model("stan_models/All_Traits_Random_Effects.stan")
   
 mods_function_all <- function(df,
-                          mod_file = "stan_models/All_Traits_Random_Effects_HS.stan",
+                          mod_file = "stan_models/All_Traits_Random_Effects.stan",
                           iter = 1000,
                           cores = 2,
                           mod = stan_model(mod_file), ...){
@@ -67,36 +62,90 @@ all_mods_noH1 = lapply(data_noH1, mods_function_all, mod = mod,
                        warmup = 1000, iter = 1500,
                        control = list(adapt_delta = 0.95)) # Divergent transitions and Bulk Effective Sample Size too low 
 
-# RMR test ------------------------------------------------------------# 
+# Estimate differences between H_nums for all traits ----------------------------
 
-out_names_full<-colnames(mk_data_full$RDMC$M)
+H_num_diff_RMR<-Hnum_difference_function(all_mods_full$ln.RMR)
+H_num_diff_RDMC<-Hnum_difference_function(all_mods_full$ln.RDMC)
+H_num_diff_SRL<-Hnum_difference_function(all_mods_full$ln.SRL)
+H_num_diff_RTD<-Hnum_difference_function(all_mods_full$ln.RTD)
+H_num_diff_SLA<-Hnum_difference_function_noH1(all_mods_noH1$ln.SLA)
+H_num_diff_LDMC<-Hnum_difference_function_noH1(all_mods_noH1$ln.LDMC)
+H_num_diff_RER<-Hnum_difference_function_noH1(all_mods_noH1$RER_ln)
+H_num_diff_RGR<-Hnum_difference_function_noH1(all_mods_noH1$RGR_Tot_ln)
 
-RMR_test<-mods_function_all(data_full$RMR, mod = mod, 
-                            pars = c("beta_Hnum_raw","beta_sp_raw", "beta_pop_raw","beta_sp_Hnum_raw"),
-                            include = FALSE, 
-                            warmup = 1000, iter = 1500, 
-                            control = list(adapt_delta = 0.95)) # Bulk effective sample size too low 
-View(summary(RMR_test)[["summary"]])
-plot(RMR_test, pars = "beta_Hnum")
+# Back transformed values for plotting --------------------------------------------
+RMR_bt_Hnum<-back_trans_function_H_num(all_mods_full$ln.RMR)
+RDMC_bt_Hnum<-back_trans_function_H_num(all_mods_full$ln.RDMC)
+SRL_bt_Hnum<-back_trans_function_H_num(all_mods_full$ln.SRL)
+RTD_bt_Hnum<-back_trans_function_H_num(all_mods_full$ln.RTD)
 
-# All mods  ----------------------------------------------------------# 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 
-View(summary(all_mods_full$RMR)[["summary"]])
-plot(all_mods_full$RMR, pars = "beta_Hnum")
 
-View(summary(all_mods_full$RDMC)[["summary"]])
-plot(all_mods_full$RDMC, pars = "beta_Hnum")
 
-View(summary(all_mods_full$SRL)[["summary"]])
-plot(all_mods_full$SRL, pars = "beta_Hnum")
 
-View(summary(all_mods_full$RTD)[["summary"]])
-plot(all_mods_full$RTD, pars = "beta_Hnum")
 
-View(summary(all_mods_noH1$SLA)[["summary"]])
-plot(all_mods_noH1$SLA, pars = "beta_Hnum")
+# Get names  ----------------------------------------------------------# 
+beta_names<-unique(Traits_all[,c("SPECIES", "H_num")])
+beta_names$names<-paste(beta_names$SPECIES, beta_names$H_num, sep = "_")
+beta_names<- beta_names[order(beta_names$names),]
+beta_names<-beta_names$names
 
-View(summary(all_mods_noH1$LDMC)[["summary"]])
-plot(all_mods_noH1$LDMC, pars = "beta_Hnum")
+beta_names_noh1<-unique(Traits_noH1[,c("SPECIES", "H_num")])
+beta_names_noh1$names_noh1<-paste(beta_names_noh1$SPECIES, beta_names_noh1$H_num, sep = "_")
+beta_names_noh1<- beta_names_noh1[order(beta_names_noh1$names_noh1),]
+beta_names_noh1<-beta_names_noh1$names_noh1
 
+# Look at results ---------------------------------------------------# 
+
+names(all_mods_full$ln.RMR)[grep("beta_sp_Hnum",names(all_mods_full$ln.RMR))]<-beta_names
+View(summary(all_mods_full$ln.RMR)[["summary"]])
+plot(all_mods_full$ln.RMR, pars = "beta_Hnum")
+plot(all_mods_full$ln.RMR, pars = "beta_sp_Hnum")
+
+names(all_mods_full$ln.RDMC)[grep("beta_sp_Hnum",names(all_mods_full$ln.RDMC))]<-beta_names
+View(summary(all_mods_full$ln.RDMC)[["summary"]])
+plot(all_mods_full$ln.RDMC, pars = "beta_Hnum")
+plot(all_mods_full$ln.RDMC, pars = "beta_sp_Hnum")
+
+names(all_mods_full$ln.SRL)[grep("beta_sp_Hnum",names(all_mods_full$ln.SRL))]<-beta_names
+View(summary(all_mods_full$ln.SRL)[["summary"]])
+plot(all_mods_full$ln.SRL, pars = "beta_Hnum")
+plot(all_mods_full$ln.SRL, pars = "beta_sp_Hnum")
+
+names(all_mods_full$ln.RTD)[grep("beta_sp_Hnum",names(all_mods_full$ln.RTD))]<-beta_names
+View(summary(all_mods_full$ln.RTD)[["summary"]])
+plot(all_mods_full$ln.RTD, pars = "beta_Hnum")
+plot(all_mods_full$ln.RTD, pars = "beta_sp_Hnum")
+
+names(all_mods_noH1$ln.SLA)[grep("beta_sp_Hnum",names(all_mods_noH1$ln.SLA))]<-beta_names_noh1
+View(summary(all_mods_noH1$ln.SLA)[["summary"]])
+plot(all_mods_noH1$ln.SLA, pars = "beta_Hnum")
+plot(all_mods_noH1$ln.SLA, pars = "beta_sp_Hnum")
+
+names(all_mods_noH1$ln.LDMC)[grep("beta_sp_Hnum",names(all_mods_noH1$ln.LDMC))]<-beta_names_noh1
+View(summary(all_mods_noH1$ln.LDMC)[["summary"]])
+plot(all_mods_noH1$ln.LDMC, pars = "beta_Hnum")
+plot(all_mods_noH1$ln.LDMC, pars = "beta_sp_Hnum")
+
+names(all_mods_noH1$RER_ln)[grep("beta_sp_Hnum",names(all_mods_noH1$RER_ln))]<-beta_names_noh1
+View(summary(all_mods_noH1$RER_ln)[["summary"]])
+plot(all_mods_noH1$RER_ln, pars = "beta_Hnum")
+plot(all_mods_noH1$RER_ln, pars = "beta_sp_Hnum")
+
+names(all_mods_noH1$RGR_Tot_ln)[grep("beta_sp_Hnum",names(all_mods_noH1$RGR_Tot_ln))]<-beta_names_noh1
+View(summary(all_mods_noH1$RGR_Tot_ln)[["summary"]])
+plot(all_mods_noH1$RGR_Tot_ln, pars = "beta_Hnum")
+plot(all_mods_noH1$RGR_Tot_ln, pars = "beta_sp_Hnum")
 
